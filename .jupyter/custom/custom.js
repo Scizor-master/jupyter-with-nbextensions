374b537173971dba98ab9a0520ffa1e72cfe5305
Scizor-master <tueda1207@gmail.com>
2018-06-30 03:44:11 +0900

    Enable keymapping of vim_binding.

--- .jupyter/custom/custom.js
+++ .jupyter/custom/custom.js
@@ -1,6 +1,6 @@
 // Configure CodeMirror Keymap
 require([
-  'nbextensions/vim_binding',   // depends your installation
+  'nbextensions/vim_binding/vim_binding',   // depends your installation
 ], function(){
   // Map jj to <Esc>
   CodeMirror.Vim.map("jj", "<Esc>", "insert");
